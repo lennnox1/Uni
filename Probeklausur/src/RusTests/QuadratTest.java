6c0af0298b5e028ec9ffc0eafba4d850cf39b4c4
Lennnox1 <pvenzke@aol.com>
2016-01-13 21:14:16 +0100

    new commit

--- Probeklausur/src/RusTests/QuadratTest.java
+++ Probeklausur/src/RusTests/QuadratTest.java
@@ -4,6 +4,9 @@ public class QuadratTest {
  public static void main(String[] args){
  Quadrat object = new Quadrat(10.0);
  double ergebnis = object.getFlaeche( );
+ Rechteck object1 = new Rechteck(10.0,15.0);
+ double ergebnisRecht = object1.getFlaeche();
  System.out.println("Die Fläche des Quadrats beträgt: " +ergebnis);
+ System.out.println("Die Fläche des Rechtecks beträgt: " +ergebnisRecht);
  }
 }
--- Probeklausur/src/RusTools/Rechteck.java
+++ Probeklausur/src/RusTools/Rechteck.java
@@ -1,9 +1,16 @@
 package RusTools;
-
-public class Rechteck {
-
-	public Rechteck() {
-		// TODO Auto-generated constructor stub
+public class Rechteck extends Quadrat {
+	/* 1) Fields */
+	private double len;
+	private double hoehe;
+	/* 2) Implementierung des Konstruktors */
+	public Rechteck(double len, double hoehe) {
+		super(len);
+		this.len = len;
+		this.hoehe = hoehe;
 	}
 
+ /* 3) Implementierung von getFlaeche( ) */
+	public double getFlaeche( ) {
+		 return this.len * this.hoehe; }
 }
